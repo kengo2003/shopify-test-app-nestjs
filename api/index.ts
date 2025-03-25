7da84fba131cbf5bca5bd49461b8517cad379bb4
kengo2003 <saitokengo2003@gmail.com>
2025-03-25 23:35:54 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -3,25 +3,21 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
 import { AppModule } from '../src/app.module';
 
-let cachedServer: express.Express | null = null;
+let cachedServer: express.Express;
 
 async function createServer(): Promise<express.Express> {
-  const expressApp = express();
+  const app = express();
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
 
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-
-  app.enableCors({
+  nestApp.enableCors({
     origin: ['https://card-function-test.myshopify.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
   });
 
-  await app.init();
-  return expressApp;
+  await nestApp.init();
+  return app;
 }
 
 export default async function handler(req: Request, res: Response) {

2ffa63a61b847cdc0b792e919971603132e833fe
kengo2003 <saitokengo2003@gmail.com>
2025-03-26 13:41:21 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -2,30 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
 import { AppModule } from '../src/app.module';
-import expressPkg from 'express/package.json';
 
-console.log('Running with express version:', expressPkg.version);
-
-let cachedServer: express.Express;
-
-async function createServer(): Promise<express.Express> {
-  const app = express();
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
-
-  nestApp.enableCors({
-    origin: ['https://card-function-test.myshopify.com'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false,
-  });
-
-  await nestApp.init();
-  return app;
-}
+let cachedApp: ReturnType<typeof express> | null = null;
 
 export default async function handler(req: Request, res: Response) {
-  if (!cachedServer) {
-    cachedServer = await createServer();
+  if (!cachedApp) {
+    const expressInstance = express();
+
+    expressInstance.use(express.json());
+    expressInstance.use(express.urlencoded({ extended: true }));
+
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressInstance),
+      {
+        logger: ['error', 'warn', 'log'],
+      },
+    );
+
+    app.enableCors({
+      origin: ['https://card-function-test.myshopify.com'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: false,
+    });
+
+    await app.init();
+    cachedApp = expressInstance;
   }
-  return cachedServer(req, res);
+
+  return cachedApp(req, res);
 }

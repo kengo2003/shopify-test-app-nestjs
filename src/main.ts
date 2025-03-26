96f59aa068abefb458e97e28dd69fef15b72fe74
kengo2003 <saitokengo2003@gmail.com>
2025-03-26 14:44:56 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   app.enableCors({
     origin: ['https://card-function-test.myshopify.com'],
-    // origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
   });
+
   await app.listen(3000);
 }
 bootstrap();

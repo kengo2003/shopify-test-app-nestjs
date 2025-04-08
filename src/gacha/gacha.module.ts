e880827295200bde339243f60ce6e1ef38bca3da
tatmius <kddiw.comusodayo@gmail.com>
2025-04-09 02:07:46 +0900

    fix missing import in gacha module

--- src/gacha/gacha.module.ts
+++ src/gacha/gacha.module.ts
@@ -3,9 +3,10 @@ import { GachaController } from './gacha.controller';
 import { GachaService } from './gacha.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RewardPointsModule } from '../points/reward-points/reward-points.module';
+import { GachaPointsModule } from '../points/gacha-points/gacha-points.module';
 
 @Module({
-  imports: [PrismaModule, RewardPointsModule],
+  imports: [PrismaModule, RewardPointsModule, GachaPointsModule],
   controllers: [GachaController],
   providers: [GachaService],
 })

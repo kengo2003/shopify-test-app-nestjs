73f97741d9a541b35014e0a061c2651e5cbab23c
tatmius <kddiw.comusodayo@gmail.com>
2025-04-08 16:01:22 +0900

    fix error

--- src/gacha/gacha.module.ts
+++ src/gacha/gacha.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GachaController } from './gacha.controller';
 import { GachaService } from './gacha.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RewardPointsModule } from '../points/reward-points/reward-points.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RewardPointsModule],
   controllers: [GachaController],
   providers: [GachaService],
 })

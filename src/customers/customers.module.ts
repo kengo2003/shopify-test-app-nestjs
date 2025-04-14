ba50ed3da9edc66421d19cafb4174ddc2ad6d4fb
tatmius <kddiw.comusodayo@gmail.com>
2025-04-14 15:09:51 +0900

    あdd prisma in customers.module

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
-
+import { PrismaModule } from '../prisma/prisma.module';
 @Module({
+  imports: [PrismaModule],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
